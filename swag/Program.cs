556e77e2efc3b447542fe0007e7bf80d19cc364b
Bianca_Reutergårdh <biareu0303@gmail.com>
2020-09-24 15:05:57 +0200

    second commit

--- swag/Program.cs
+++ swag/Program.cs
@@ -8,75 +8,127 @@ namespace swag
         {
             int points = 0;
             int totalpoints = 3;
-            string answer1 = "a";
-            string answer2 = "c";
-            string answer3 = "c";
             string playeranswer1 = "";
             string playeranswer2 = "";
             string playeranswer3 = "";
-
+            int a = 1;
+            int b = 1;
+            int c = 1;
+            string restart = "";
 
             Console.WriteLine("Här är Min frågesport om min hund:");
 
-            Console.WriteLine("Vad heter hon?  A. Nelly  B.Shanti  C.Chelsea");
-            playeranswer1 = Console.ReadLine().ToLower();
+            while (a == 1)
+            {
 
-            if (playeranswer1 == "a")
-            {
-                Console.WriteLine("Rätt mannen!");
-                points++;
 
-            }
-            else if (playeranswer1 == "b" || playeranswer1 == "c")
-            {
-                Console.WriteLine("FEL SVAR");
-            }
-            else
-            {
-                Console.WriteLine("Fel input, välj mellan A, B eller C");
+                Console.WriteLine("Vad heter hon?  A. Nelly  B.Shanti  C.Chelsea");
+                playeranswer1 = Console.ReadLine().ToLower();
+
+                if (playeranswer1 == "a")
+                {
+                    Console.WriteLine("Rätt mannen!");
+                    points++;
+                    a = a - 1;
+
+                }
+                else if (playeranswer1 == "b" || playeranswer1 == "c")
+                {
+                    Console.WriteLine("FEL SVAR");
+                    a = a - 1;
+                }
+                else
+                {
+                    Console.WriteLine("Fel input, välj mellan A, B eller C");
+                }
             }
 
-            Console.WriteLine("Alright next question: ");
-            Console.WriteLine("Hur gammal är min hund?  A. 5  B. 10  C. 12");
+            while (b == 1)
+            {
+                Console.WriteLine("Alright next question: ");
 
-            playeranswer2 = Console.ReadLine().ToLower();
+                Console.WriteLine("Hur gammal är min hund?  A. 5  B. 10  C. 12");
 
-            if (playeranswer2 == "c")
-            {
-                Console.WriteLine("Good job dud, d va rätt!");
-                points++;
-            }
-            else if (playeranswer2 == "a" || playeranswer2 == "b")
-            {
-                Console.WriteLine("Dumb hoe kan inte ens enklaste frågan... ");
-            }
-            else
-            {
-                Console.WriteLine("Fel input, välj mellan A, B eller C");
+                playeranswer2 = Console.ReadLine().ToLower();
+
+                if (playeranswer2 == "c")
+                {
+                    Console.WriteLine("Good job dud, d va rätt!");
+                    points++;
+                    b = b - 1;
+                }
+                else if (playeranswer2 == "a" || playeranswer2 == "b")
+                {
+                    Console.WriteLine("Dumb hoe kan inte ens enklaste frågan... ");
+                    b = b - 1;
+                }
+                else
+                {
+                    Console.WriteLine("Fel input, välj mellan A, B eller C");
+                }
+
             }
 
             Console.WriteLine("Okidoki bror du e on a rollllll, lets get it nästa: ");
-            Console.WriteLine("Hur mycket väger mi hund?  A. 4kg  B. 5kg  C. 3kg");
 
-            playeranswer3 = Console.ReadLine().ToLower();
-
-            if (playeranswer3 == "c")
+            while (c == 1)
             {
-                Console.WriteLine("Fuck do I know inte ens jag vet, men nått sånt gissar jag. Bra gjort!");
-                points++;
+                Console.WriteLine("Hur mycket väger mi hund?  A. 4kg  B. 5kg  C. 3kg");
+
+                playeranswer3 = Console.ReadLine().ToLower();
+
+                if (playeranswer3 == "c")
+                {
+                    Console.WriteLine("Fuck do I know inte ens jag vet, men nått sånt gissar jag. Bra gjort!");
+                    points++;
+                    c = c - 1;
+                }
+                else if (playeranswer3 == "a" || playeranswer3 == "b")
+                {
+                    Console.WriteLine("Åååååååå du ÄR så dum. Du fick fel.");
+                    c = c - 1;
+                }
+                else
+                {
+                    Console.WriteLine("Fel input, välj mellan A, B eller C");
+                }
             }
-            else if (playeranswer3 == "a" || playeranswer3 == "b")
+
+            if (points == 0 | points == 1)
             {
-                Console.WriteLine("Åååååååå du ÄR så dum. Du fick fel.");
+                Console.WriteLine("Is you dumb stupid, or dumb. You got " + points + " points out of " + totalpoints + ".");
+
+            }
+            else if (points == 2)
+            {
+                Console.WriteLine("You kinda smart bro good job. You got " + points + " points out of " + totalpoints + ".");
+
+            }
+
+            else
+            {
+                Console.WriteLine("You a damn genius bro daaamn ayy. You got " + points + " points out of " + totalpoints + ".");
+            }
+
+            Console.WriteLine("Vill du spel igen? Y/N");
+            restart = Console.ReadLine().ToLower();
+
+            if (restart == "y")
+            {
+                a = 1;
+                b = 2;
+                c = 3;
+            }
+            else if (restart == "n")
+            {
+                Console.WriteLine("Okej tack för att du spelade!");
+                Console.ReadLine();
             }
             else
             {
-                Console.WriteLine("Fel input, välj mellan A, B eller C");
+                Console.WriteLine("Incorrect input!");
+                Console.ReadLine();
             }
-
-            Console.WriteLine("You got " + points + " points out of " + totalpoints + ".");
-
-            Console.WriteLine("Vill du spel igen???????");
             Console.ReadLine();
         }
     }
